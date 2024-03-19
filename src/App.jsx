19f685722e2b159aa1e7b3a6e9febd8768b99ee4
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-03-18 20:48:20 -0600

    Modo dark casi funcional

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div  className='h-screen flex justify-center items-center dark:bg-neutral-900'>
+      
+      <button className='bg-slate-400 px-4 py-2 rounded hover:bg-purple-400'>
+        Cambiar tema
+      </button>
+    </div>
+  );
+}
+
+export default App;
