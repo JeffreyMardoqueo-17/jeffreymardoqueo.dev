54f28cf6f32f54639a1b0950d40da64fc38d4cc6
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-03-19 10:58:06 -0600

    Aqui ya funciona el medo dark y light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
-  "darkMode": "class",
-  plugins: [],
-}
-
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        darkPurple: "#280133",
+        lightPurple: "#ffff",
+        lightText: "#280133",
+        hoverBG: "#e4b6ffc7", 
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ["dark"],
+    },
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+};
