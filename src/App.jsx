e8a3486677f649cd4c1a98640894778fe6d6b7f0
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-03-19 21:39:07 -0600

    Fucniona el modo dark pero me reinicia la pagina y no quierio eso

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Menu from './components/Menu/Menu';
 import Welcome from './components/welcome/welcome';
+import AboutMe from './components/Aboutme/AboutMe';
 
 const App = () => {
-
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       {/* Menú con ancho fijo de 400px */}
       <div className="w-20 h-screen fixed overflow-y-auto">
         <Menu />
       </div>
       {/* Contenido restante */}
       <div className="flex-1 ml-20 h-full flex justify-center items-top bg-lightPurple dark:bg-darkPurple">
-       <Welcome />
+        <div className="flex flex-col">
+          <Welcome />
+          <AboutMe />
+          {/* Agrega más componentes Welcome aquí si deseas */}
+        </div>
       </div>
     </div>
   );
 };
-
 export default App;
