916bbee38ba09bb0d912fa07551f5e9188d4db6c
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-04-25 12:57:41 -0600

    Habilidades listas

--- src/Pages/TechnologiesPage.jsx
+++ src/Pages/TechnologiesPage.jsx
@@ -1,61 +1,57 @@
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiCsharp } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
-import { FaBootstrap } from "react-icons/fa6";
+import { FaBootstrap } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa";
 import { FaSass } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
 import { FaCss3Alt } from "react-icons/fa";
 import { FaNode } from "react-icons/fa";
 import { SiMicrosoftsqlserver } from "react-icons/si";
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 import React, { useState } from 'react';
 
 const TechnologiesPage = () => {
     const [selectedTech, setSelectedTech] = useState(null);
 
     const handleIconClick = (techName) => {
-        setSelectedTech(techName);
+        setSelectedTech(techName === selectedTech ? null : techName);
     };
 
     const technologies = [
-        { name: 'JavaScript', icon: <IoLogoJavascript className="text-warning-500 w-24 h-24 " /> },
-        { name: 'React', icon: <FaReact className="text-blue-600 w-24 h-24 " /> },
-        { name: 'HTML', icon: <FaHtml5 className="text-orange-500 w-24 h-24 " /> },
-        { name: 'Sass', icon: <FaSass className="text-pink-500 w-24 h-24 " /> },
-        { name: 'Tailwindcss ', icon: <SiTailwindcss className="text-blue-500 w-24 h-24 " /> },
-        { name: 'CSS', icon: <FaCss3Alt className="text-blue-600 w-24 h-24 " /> },
-        { name: 'Node JS', icon: <FaNode className="text-green-600 w-24 h-24 " /> },
-        { name: 'SQL Server', icon: <SiMicrosoftsqlserver className="text-red-600 w-24 h-24 " /> },
-        { name: 'Bootstrap', icon: <FaBootstrap className="text-purple-800 w-24 h-24" /> },
-        { name: 'C Sharp', icon: <SiCsharp className="text-purple-700 w-24 h-24" /> },
-        { name: 'GitHub', icon: <FaGithub className="bg-black text-white w-24 h-24 rounded-full p-3" /> }
+        { name: 'JavaScript', icon: <IoLogoJavascript className="text-warning-500 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'React', icon: <FaReact className="text-blue-600 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'HTML', icon: <FaHtml5 className="text-orange-500 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'Sass', icon: <FaSass className="text-pink-500 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'Tailwindcss', icon: <SiTailwindcss className="text-blue-500 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'CSS', icon: <FaCss3Alt className="text-blue-600 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'Node JS', icon: <FaNode className="text-green-600 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'SQL Server', icon: <SiMicrosoftsqlserver className="text-red-600 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'Bootstrap', icon: <FaBootstrap className="text-purple-800 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'C Sharp', icon: <SiCsharp className="text-purple-700 w-24 h-24 absolute inset-0 m-auto" /> },
+        { name: 'GitHub', icon: <FaGithub className="bg-black text-white w-24 h-24 rounded-full p-3 absolute inset-0 m-auto" /> }
         // Agrega más tecnologías según sea necesario
     ];
 
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Tecnologías que manejo</h1>
+             <h2 className="text-3xl text-center mb-6 font-bold leading-tight dark:text-white text-darkPurple sm:text-4xl lg:text-5xl">Habilidades 
+                            <br className="block sm:hidden" /> <span className=' text-hoverBG'>Relevantes </span>
+
+                            {/* AGREGAR UNA SESION DONDE PUEDA SELECCIONAR EL USAURIO Y DECIR COMO ESTA */}
+                        </h2>
             <ul className="grid grid-cols-3 gap-4">
                 {technologies.map((tech, index) => (
-                    <li key={index} className="relative">
-                        <div
-                            className="absolute top-0 left-0 right-0 bottom-0 bg-white bg-opacity-75 flex justify-center items-center opacity-0 transition-opacity duration-300 hover:opacity-100"
-                            onClick={() => handleIconClick(tech.name)}
-                            onMouseEnter={() => setSelectedTech(tech.name)}
-                            onMouseLeave={() => setSelectedTech(null)}
-                        >
-                            <p className="text-lg font-bold">{tech.name}</p>
-                        </div>
-                        <div
-                            className="text-center cursor-pointer"
-                        >
+                    <li key={index} className="relative group" onClick={() => handleIconClick(tech.name)} onMouseEnter={() => setSelectedTech(tech.name)} onMouseLeave={() => setSelectedTech(null)}>
+                        <div className="w-24 h-24 flex items-center justify-center">
                             {tech.icon}
                         </div>
+                        {selectedTech === tech.name && (
+                            <p className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-white bg-gray-600 bg-opacity-75 rounded">{tech.name}</p>
+                        )}
                     </li>
                 ))}
             </ul>
-            {selectedTech && <p className="mt-4 text-xl">{selectedTech}</p>}
         </div>
     );
 };
--- src/components/Estudio/Estudio.jsx
+++ src/components/Estudio/Estudio.jsx
@@ -3,13 +3,28 @@ import React from 'react';
 const Estudio = () => {
     return (
         <div className='ml-2'>
-
-            <ol class="relative border-s border-hoverBG">
+            <h1 className='text-3xl font-bold mb-5 text-redPasion'>Educacion Superior</h1>
+            <ol class="relative border-s ml-9 border-hoverBG">
                 <li class="mb-10 ms-4">
                     <div class="absolute w-3 h-3 bg-rojoClaro rounded-full mt-1.5 -start-1.5 border border-hoverBG "></div>
-                    <time class="mb-1 text-xl font-normal leading-none text-rojoClaro">February 2022</time>
-                    <h3 class="text-2xl font-semibold text-darkPurple dark:text-lightPurple">Application UI code in Tailwind CSS</h3>
-                    <p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+                    <time class="mb-1 text-xl font-normal leading-none text-rojoClaro">ESFE/AGAPE 2023 - 2024</time>
+                    <h3 class="text-2xl font-semibold text-darkPurple dark:text-lightPurple">Técnico En Ingeniería de Desarrollo de Software</h3>
+                    <p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Diseñar, desarrollar y mantener software, asegurando su correcto funcionamiento y adaptándolo a las necesidades de los usuarios.</p>
+                </li>
+            </ol>
+             <h1 className='text-3xl font-bold mb-5 text-redPasion'>Otros</h1>
+            <ol class="relative border-s ml-9 border-hoverBG">
+                <li class="mb-10 ms-4">
+                    <div class="absolute w-3 h-3 bg-rojoClaro rounded-full mt-1.5 -start-1.5 border border-hoverBG "></div>
+                    <time class="mb-1 text-xl font-normal leading-none text-rojoClaro">TPC Sonsonate, 2022</time>
+                    <h3 class="text-2xl font-semibold text-darkPurple dark:text-lightPurple">Técnico Programador Analista de Sistemas</h3>
+                    <p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Diseñar, desarrollar, implementar y mantener sistemas informáticos</p>
+                </li>
+                <li class="mb-10 ms-4">
+                    <div class="absolute w-3 h-3 bg-rojoClaro rounded-full mt-1.5 -start-1.5 border border-hoverBG "></div>
+                    <time class="mb-1 text-xl font-normal leading-none text-rojoClaro">TPC Sonsonate, 2021</time>
+                    <h3 class="text-2xl font-semibold text-darkPurple dark:text-lightPurple">Técnico Operador en Windows</h3>
+                    <p class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400">Administración y operación de sistemas Windows, manejo de programas del paquete de Office.</p>
                 </li>
             </ol>
         </div>
