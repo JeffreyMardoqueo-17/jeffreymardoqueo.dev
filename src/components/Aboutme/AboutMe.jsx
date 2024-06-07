5ef44ede9afee0bfbce98788ab1aef33a344636b
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-07 14:11:07 -0600

    Sobre mi avout me

--- src/components/Aboutme/AboutMe.jsx
+++ src/components/Aboutme/AboutMe.jsx
@@ -17,24 +17,62 @@ const AboutMe = () => {
                         <div className="block lg:hidden rounded-full mx-auto h-32 w-32 bg-cover bg-center" style={{ backgroundImage: "url('/imgs/peer.png')" }}></div>
                         <h1 className='text-3xl font-bold mb-5 text-redPasion'>Sobre mi</h1>
                         {/* Rest of the content... */}
-                        <p className="mt-4 dark:text-gray-100 text-justify">
-                            Actualmente soy un estudiante con grandes ambiciones de crecer en el ámbito del desarrollo de software. Mi objetivo es ser arquitecto de software. Tengo 18 años y siempre busco estar aprendiendo cosas nuevas y esforzándome por lo que quiero.
+
+                        <p className="mt-4 ext-gray-900 dark:text-gray-400 text-justify">
+                            Actualmente soy un estudiante con grandes ambiciones de crecer en el ámbito del  <strong className="font-semibold text-gray-900 dark:text-white font-bold">Desarrollo de software</strong> Mi objetivo es ser arquitecto de software. Tengo 18 años y siempre busco estar aprendiendo cosas nuevas y esforzándome por lo que quiero.
                         </p>
                         <p className="mt-4 dark:text-gray-100 text-justify">
-                            <strong>Cualidades:</strong> Perseverante, detallista y proactivo. Me esfuerzo por mantenerme actualizado con las últimas tecnologías y mejores prácticas en el desarrollo de software.
+                            <strong className='dark:text-white'>Calidades:</strong> Perseverante, detallista y proactivo. Me esfuerzo por mantenerme actualizado con las últimas tecnologías y mejores prácticas en el desarrollo de software.
                         </p>
+
                         <p className="mt-4 dark:text-gray-100 text-justify">
                             <strong>Habilidades sociales:</strong>
-                            <ul className="list-disc list-inside text-justify">
-                                <li>Soy colaborador y me gusta trabajar en equipo.</li>
-                                <li>Disfruto resolviendo problemas y encontrando soluciones eficientes.</li>
-                                <li>Tengo habilidades de liderazgo y me esfuerzo por motivar a mi equipo a alcanzar sus objetivos.</li>
-                                <li>Soy empático y me esfuerzo por entender las perspectivas de los demás.</li>
-                                <li>Tengo una actitud positiva y trato de inspirar a los demás con mi entusiasmo.</li>
-                                <li>Soy un buen comunicador y me aseguro de que todos en el equipo estén alineados y comprendan sus roles y tareas.</li>
-                                <li>Soy flexible y me adapto bien a diferentes situaciones y cambios.</li>
+                            <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Soy colaborador y me gusta trabajar en equipo.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Disfruto resolviendo problemas y encontrando soluciones eficientes.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Tengo habilidades de liderazgo y me esfuerzo por motivar a mi equipo a alcanzar sus objetivos.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Soy empático y me esfuerzo por entender las perspectivas de los demás.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Tengo una actitud positiva y trato de inspirar a los demás con mi entusiasmo.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Soy un buen comunicador y me aseguro de que todos en el equipo estén alineados y comprendan sus roles y tareas.</span>
+                                </li>
+                                <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                    </svg>
+                                    <span>Soy flexible y me adapto bien a diferentes situaciones y cambios.</span>
+                                </li>
                             </ul>
                         </p>
+
                         <p className="mt-4 dark:text-gray-100 text-justify">
                             Aquí puedes ver más adelante parte de mis conocimientos. ¡CONTINÚA VIENDO MI PORTAFOLIO!
                         </p>
