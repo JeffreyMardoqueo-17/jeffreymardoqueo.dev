4089f2ce7bcb85cbc9fdecdb25117e4cfec2796d
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-06-07 14:16:01 -0600

    About casi liskto

--- src/components/Aboutme/AboutMe.jsx
+++ src/components/Aboutme/AboutMe.jsx
@@ -18,16 +18,16 @@ const AboutMe = () => {
                         <h1 className='text-3xl font-bold mb-5 text-redPasion'>Sobre mi</h1>
                         {/* Rest of the content... */}
 
-                        <p className="mt-4 ext-gray-900 dark:text-gray-400 text-justify">
+                        <p className="mt-4 text-gray-900 dark:text-gray-100 text-justify">
                             Actualmente soy un estudiante con grandes ambiciones de crecer en el ámbito del  <strong className="font-semibold text-gray-900 dark:text-white font-bold">Desarrollo de software</strong> Mi objetivo es ser arquitecto de software. Tengo 18 años y siempre busco estar aprendiendo cosas nuevas y esforzándome por lo que quiero.
                         </p>
                         <p className="mt-4 dark:text-gray-100 text-justify">
                             <strong className='dark:text-white'>Calidades:</strong> Perseverante, detallista y proactivo. Me esfuerzo por mantenerme actualizado con las últimas tecnologías y mejores prácticas en el desarrollo de software.
                         </p>
 
-                        <p className="mt-4 dark:text-gray-100 text-justify">
+                        <p className="mt-4 text-justify">
                             <strong>Habilidades sociales:</strong>
-                            <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+                            <ul className="space-y-4 text-left text-black dark:text-gray-100">
                                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
                                     <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
